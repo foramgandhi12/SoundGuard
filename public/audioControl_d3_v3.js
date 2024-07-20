cdb4a9a548948496db011489d3eb7224c8a8be1c
John McSwain <john.i.mcswain@gmail.com>
2024-07-20 11:39:31 -0400

    adding pwa code and code caching service workers, app and favicons

--- public/audioControl_d3_v3.js
+++ public/audioControl_d3_v3.js
@@ -1,72 +1,71 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js";
-import firebaseConfig from "./firebaseConfig.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js'
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
+import firebaseConfig from './firebaseConfig.js'
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
-let audioContext;
-let analyser;
-let dataArray;
-let source;
-let stream;
-let isMicOn = false;
-const data = [];
-const maxDataPoints = 300;
-let notificationVisible = false;
+let audioContext
+let analyser
+let dataArray
+let source
+let stream
+let isMicOn = false
+const data = []
+const maxDataPoints = 300
+let notificationVisible = false
 
-const svg = d3.select(".graph-container");
-const width = +svg.attr("width");
-const height = +svg.attr("height");
-const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-const innerWidth = width - margin.left - margin.right;
-const innerHeight = height - margin.top - margin.bottom;
+const svg = d3.select('.graph-container')
+const width = +svg.attr('width')
+const height = +svg.attr('height')
+const margin = { top: 20, right: 20, bottom: 30, left: 50 }
+const innerWidth = width - margin.left - margin.right
+const innerHeight = height - margin.top - margin.bottom
 
-const xScale = d3.scaleLinear().range([0, innerWidth]);
-const yScale = d3.scaleLinear().domain([0, 140]).range([innerHeight, 0]);
+const xScale = d3.scaleLinear().range([0, innerWidth])
+const yScale = d3.scaleLinear().domain([0, 140]).range([innerHeight, 0])
 
 const line = d3
   .line()
   .x((d, i) => xScale(i))
-  .y((d) => yScale(d.volume));
+  .y((d) => yScale(d.volume))
 
 const g = svg
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  .append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`)
 
-g.append("g")
-  .attr("class", "x-axis")
-  .attr("transform", `translate(0,${innerHeight})`);
+g.append('g')
+  .attr('class', 'x-axis')
+  .attr('transform', `translate(0,${innerHeight})`)
 
-g.append("g").attr("class", "y-axis");
+g.append('g').attr('class', 'y-axis')
 
-function updateChart(treshold) {
-  xScale.domain([0, maxDataPoints - 1]);
+function updateChart (treshold) {
+  xScale.domain([0, maxDataPoints - 1])
 
   const segments = data.map((d, i) => ({
     x1: xScale(i),
     y1: yScale(d.volume),
     x2: xScale(i + 1),
     y2: i < data.length - 1 ? yScale(data[i + 1].volume) : yScale(d.volume),
-    color: d.volume <= treshold/2 ? "#238823" : d.volume <= treshold ? "#EFB700" : "#D2222D",
-  }));
+    color: d.volume <= treshold / 2 ? '#238823' : d.volume <= treshold ? '#EFB700' : '#D2222D'
+  }))
 
-  const lines = g.selectAll(".segment").data(segments);
+  const lines = g.selectAll('.segment').data(segments)
 
   lines
     .enter()
-    .append("line")
-    .attr("class", "segment")
+    .append('line')
+    .attr('class', 'segment')
     .merge(lines)
-    .attr("x1", (d) => d.x1)
-    .attr("y1", (d) => d.y1)
-    .attr("x2", (d) => d.x2)
-    .attr("y2", (d) => d.y2)
-    .attr("stroke", (d) => d.color)
-    .attr("stroke-width", 2);
+    .attr('x1', (d) => d.x1)
+    .attr('y1', (d) => d.y1)
+    .attr('x2', (d) => d.x2)
+    .attr('y2', (d) => d.y2)
+    .attr('stroke', (d) => d.color)
+    .attr('stroke-width', 2)
 
-  lines.exit().remove();
+  lines.exit().remove()
 
   // Removed x-axis for better graph visual
   // g.select(".x-axis").call(
@@ -78,113 +77,113 @@ function updateChart(treshold) {
   //     ),
   // );
 
-  g.select(".y-axis").call(d3.axisLeft(yScale));
+  g.select('.y-axis').call(d3.axisLeft(yScale))
 }
 
-//bootstrap component retrieved from https://getbootstrap.com/docs/5.3/components/alerts/
-const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+// bootstrap component retrieved from https://getbootstrap.com/docs/5.3/components/alerts/
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message) => {
-  notificationVisible = true;
-  const wrapper = document.createElement("div");
+  notificationVisible = true
+  const wrapper = document.createElement('div')
   wrapper.innerHTML = [
-    `<div class="alert alert-danger alert-dismissible" role="alert">`,
-        `<div> Loud Enviroment! </div>`,
+    '<div class="alert alert-danger alert-dismissible" role="alert">',
+    '<div> Loud Enviroment! </div>',
     `   <div>${message}</div>`,
     '   <button type="button" class="btn-close" data-bs-dismiss="alert"></button>',
-    "</div>",
-  ].join("");
+    '</div>'
+  ].join('')
 
-  wrapper.querySelector(".btn-close").addEventListener("click", () => {
-    notificationVisible = false;
-  });
+  wrapper.querySelector('.btn-close').addEventListener('click', () => {
+    notificationVisible = false
+  })
 
-  alertPlaceholder.append(wrapper);
-};
+  alertPlaceholder.append(wrapper)
+}
 
-async function* updateVolumeGenerator(threshold) {
+async function * updateVolumeGenerator (threshold) {
   while (isMicOn) {
-    analyser.getByteFrequencyData(dataArray);
-    let sum = 0;
+    analyser.getByteFrequencyData(dataArray)
+    let sum = 0
     for (let i = 0; i < dataArray.length; i++) {
-      sum += dataArray[i];
+      sum += dataArray[i]
     }
-    const averageVolume = sum / dataArray.length;
-    const roundedVolume = Math.round(averageVolume);
+    const averageVolume = sum / dataArray.length
+    const roundedVolume = Math.round(averageVolume)
 
-    document.getElementById("volume").innerText = `Volume: ${roundedVolume} dB`;
-    if (data.length >= maxDataPoints) data.shift();
-    data.push({ time: new Date(), volume: roundedVolume });
+    document.getElementById('volume').innerText = `Volume: ${roundedVolume} dB`
+    if (data.length >= maxDataPoints) data.shift()
+    data.push({ time: new Date(), volume: roundedVolume })
 
     if (roundedVolume > threshold && !notificationVisible) {
-      appendAlert(`Noise level is above ${threshold} dB`);
-      storeVolumeViolation(roundedVolume);
+      appendAlert(`Noise level is above ${threshold} dB`)
+      storeVolumeViolation(roundedVolume)
     }
-    
-    updateChart(threshold);
 
-    await new Promise((resolve) => setTimeout(resolve, 1000 / 60)); // Update at ~60 FPS
-    yield;
+    updateChart(threshold)
+
+    await new Promise((resolve) => setTimeout(resolve, 1000 / 60)) // Update at ~60 FPS
+    yield
   }
 }
 
-async function storeVolumeViolation(volume) {
+async function storeVolumeViolation (volume) {
   try {
-    await addDoc(collection(db, "violations"), {
+    await addDoc(collection(db, 'violations'), {
       time: new Date(),
-      volume: volume,
-    });
-    console.log("Volume violation stored:", volume);
+      volume
+    })
+    console.log('Volume violation stored:', volume)
   } catch (e) {
-    console.error("Error adding document: ", e);
+    console.error('Error adding document: ', e)
   }
 }
 
-async function startMicrophone() {
+async function startMicrophone () {
   try {
-    stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    source = audioContext.createMediaStreamSource(stream);
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 256;
-    source.connect(analyser);
-    dataArray = new Uint8Array(analyser.frequencyBinCount);
-    let threshold = document.getElementById("current-threshold").value;
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+    audioContext = new (window.AudioContext || window.webkitAudioContext)()
+    source = audioContext.createMediaStreamSource(stream)
+    analyser = audioContext.createAnalyser()
+    analyser.fftSize = 256
+    source.connect(analyser)
+    dataArray = new Uint8Array(analyser.frequencyBinCount)
+    const threshold = document.getElementById('current-threshold').value
 
-    const volumeGenerator = updateVolumeGenerator(threshold);
-    async function runGenerator() {
+    const volumeGenerator = updateVolumeGenerator(threshold)
+    async function runGenerator () {
       for await (const _ of volumeGenerator) {
       }
     }
 
-    runGenerator();
+    runGenerator()
   } catch (err) {
-    console.error("Error accessing microphone:", err);
+    console.error('Error accessing microphone:', err)
   }
 }
 
-function stopMicrophone() {
+function stopMicrophone () {
   if (stream) {
-    stream.getTracks().forEach((track) => track.stop());
+    stream.getTracks().forEach((track) => track.stop())
   }
   if (audioContext) {
-    audioContext.close();
+    audioContext.close()
   }
-  isMicOn = false;
-  document.getElementById("volume").innerText = "Volume: 0 dB";
-  updateChart();
+  isMicOn = false
+  document.getElementById('volume').innerText = 'Volume: 0 dB'
+  updateChart()
 }
 
-document.getElementById("toggle-mic").addEventListener("click", function () {
+document.getElementById('toggle-mic').addEventListener('click', function () {
   if (isMicOn) {
-    stopMicrophone();
+    stopMicrophone()
     this.innerHTML =
-      '<i class="bi bi-mic-fill"></i> <span id="toggle-mic-text">Start Microphone</span>';
-    this.className = "btn btn-dark mb-4";
+      '<i class="bi bi-mic-fill"></i> <span id="toggle-mic-text">Start Microphone</span>'
+    this.className = 'btn btn-dark mb-4'
   } else {
-    isMicOn = true;
-    startMicrophone();
+    isMicOn = true
+    startMicrophone()
     this.innerHTML =
-      '<i class="bi bi-mic-fill" ></i> <span id="toggle-mic-text">Stop Microphone</span>';
-    this.className = "btn btn-secondary mb-4";
+      '<i class="bi bi-mic-fill" ></i> <span id="toggle-mic-text">Stop Microphone</span>'
+    this.className = 'btn btn-secondary mb-4'
   }
-});
+})
--- public/auth.js
+++ public/auth.js
@@ -1,30 +1,30 @@
 // auth.js
 
-import { Auth0Client } from 'https://cdn.jsdelivr.net/npm/@auth0/auth0-spa-js@1.20.0/dist/auth0-spa-js.production.esm.js';
-import auth0Config from './auth0Config.js';
+import { Auth0Client } from 'https://cdn.jsdelivr.net/npm/@auth0/auth0-spa-js@1.20.0/dist/auth0-spa-js.production.esm.js'
+import auth0Config from './auth0Config.js'
 
-let auth0Client = null;
+let auth0Client = null
 
-async function createClient() {
+async function createClient () {
   auth0Client = new Auth0Client({
     domain: auth0Config.domain,
     client_id: auth0Config.clientId,
     redirect_uri: auth0Config.redirectUri
-  });
+  })
 }
 
-async function login() {
-    console.log(auth0Client);
-  await auth0Client.loginWithRedirect();
+async function login () {
+  console.log(auth0Client)
+  await auth0Client.loginWithRedirect()
 }
 
-async function logout() {
-    console.log(auth0Client);
-  await auth0Client.logout();
+async function logout () {
+  console.log(auth0Client)
+  await auth0Client.logout()
 }
 
-async function getUser() {
-  return await auth0Client.getUser();
+async function getUser () {
+  return await auth0Client.getUser()
 }
 
-export { createClient, login, logout, getUser };
+export { createClient, login, logout, getUser }
